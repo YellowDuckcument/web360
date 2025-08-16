aad1067e781088113e2c5cfc1a3a5006a5ea38a9
Tins <lutin651@gmail.com>
2025-08-16 15:07:17 +0700

    Add Potree

--- src/pages/PointCloud/PointCloud.jsx
+++ src/pages/PointCloud/PointCloud.jsx
@@ -1,36 +1,21 @@
-import Potree from "../PointCloud/Potree"
+import Potree from "../PointCloud/Potree";
 
 function PointCloud() {
   return (
-        //   <iframe
-        //       src="/PotreeViewer.html"
-        //       title="Potree Viewer"
-        //       width="100%"
-        //       height="100%"
-        //       style={{
-        //         position: "fixed",
-        //         top: 58,
-        //         left: 0,
-        //         border: "none",
-        //         width: "100vw",
-        //         height: "calc(100vh - 58px)",
-        //         zIndex: 0,
-        //     }}
-        //   />
-
-          <Potree 
-          width="100%"
-          height="100%"
-          style={{
-                position: "fixed",
-                top: 58,
-                left: 0,
-                border: "none",
-                width: "100vw",
-                height: "calc(100vh - 58px)",
-                zIndex: 0,
-            }}/>
-      );
+    <Potree
+      width="100%"
+      height="100%"
+      style={{
+        position: "fixed",
+        top: 58,
+        left: 0,
+        border: "none",
+        width: "100vw",
+        height: "calc(100vh - 58px)",
+        zIndex: 0,
+      }}
+    />
+  );
 }
 
 export default PointCloud;
--- src/pages/PointCloud/Potree.jsx
+++ src/pages/PointCloud/Potree.jsx
@@ -30,33 +30,35 @@ function Potree() {
     const initPotree = async () => {
       try {
         const cssList = [
-          "/libs/buildPotree/potree/potree.css",
-          "/libs/openlayers3/ol.css",
-          "/libs/spectrum/spectrum.css",
-          "/libs/jstree/themes/mixed/style.css",
-          "/libs/jquery-ui/jquery-ui.min.css",
+          "./libs/buildPotree/potree/potree.css",
+          "./libs/openlayers3/ol.css",
+          "./libs/spectrum/spectrum.css",
+          "./libs/jstree/themes/mixed/style.css",
+          "./libs/jquery-ui/jquery-ui.min.css",
         ];
         await Promise.all(cssList.map(loadCSS));
 
         const scriptList = [
-          "/libs/jquery/jquery-3.1.1.min.js",
-          "/libs/spectrum/spectrum.js",
-          "/libs/jquery-ui/jquery-ui.min.js",
-          "/libs/other/BinaryHeap.js",
-          "/libs/tween/tween.min.js",
-          "/libs/d3/d3.js",
-          "/libs/proj4/proj4.js",
-          "/libs/openlayers3/ol.js",
-          "/libs/i18next/i18next.js",
-          "/libs/jstree/jstree.js",
-          "/libs/buildPotree/potree/potree.js",
-          "/libs/plasio/js/laslaz.js",
+          "./libs/jquery/jquery-3.1.1.min.js",
+          "./libs/spectrum/spectrum.js",
+          "./libs/jquery-ui/jquery-ui.min.js",
+          "./libs/other/BinaryHeap.js",
+          "./libs/tween/tween.min.js",
+          "./libs/d3/d3.js",
+          "./libs/proj4/proj4.js",
+          "./libs/openlayers3/ol.js",
+          "./libs/i18next/i18next.js",
+          "./libs/jstree/jstree.js",
+          "./libs/buildPotree/potree/potree.js",
+          "./libs/plasio/js/laslaz.js",
         ];
         for (const src of scriptList) await loadScript(src);
 
         // Khởi tạo viewer 1 lần duy nhất
         if (!viewerRef.current) {
-          const viewer = new window.Potree.Viewer(document.getElementById("potree_render_area"));
+          const viewer = new window.Potree.Viewer(
+            document.getElementById("potree_render_area")
+          );
           viewerRef.current = viewer;
 
           viewer.setEDLEnabled(true);
@@ -75,23 +77,49 @@ function Potree() {
           });
 
           // Load Pointcloud
-          window.Potree.loadPointCloud(
-            "/models/pointcloud/test02/metadata.json",
-            "Tòa nhà Gtel",
-            (e) => {
-              viewer.scene.addPointCloud(e.pointcloud);
+          // window.Potree.loadPointCloud(
+          //   "/KTX/models/pointcloud/test2/metadata.json",
+          //   "Tòa nhà Gtel",
+          //   (e) => {
+          //     viewer.scene.addPointCloud(e.pointcloud);
 
-              const material = e.pointcloud.material;
-              material.size = 1;
-              material.pointSizeType = window.Potree.PointSizeType.ADAPTIVE;
+          //     const material = e.pointcloud.material;
+          //     material.size = 1;
+          //     material.pointSizeType = window.Potree.PointSizeType.ADAPTIVE;
 
-              e.pointcloud.position.x += 3;
-              e.pointcloud.position.y -= 3;
-              e.pointcloud.position.z += 4;
+          //     e.pointcloud.position.x += 3;
+          //     e.pointcloud.position.y -= 3;
+          //     e.pointcloud.position.z += 4;
 
-              viewer.fitToScreen();
-            }
-          );
+          //     viewer.fitToScreen();
+          //   }
+          // );
+
+          const pointCloudList = [
+            {
+              url: "/KTX/models/pointcloud/test1/metadata.json",
+              name: "Toà nhà 1",
+              position: [0, 0, 0],
+            },
+            {
+              url: "/KTX/models/pointcloud/test2/metadata.json",
+              name: "Toà nhà 2",
+              position: [10, 0, 0],
+            },
+          ];
+
+          pointCloudList.forEach((item) => {
+            window.Potree.loadPointCloud(item.url, item.name, (e) => {
+              const pc = e.pointcloud;
+              viewer.scene.addPointCloud(pc);
+
+              pc.material.size = 1;
+              pc.material.pointSizeType = window.Potree.PointSizeType.ADAPTIVE;
+
+              // Set position
+              pc.position.set(...item.position);
+            });
+          });
 
           // Thêm ánh sáng
           const directional = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -115,9 +143,12 @@ function Potree() {
       className="potree_container"
       style={{ position: "absolute", width: "100%", height: "100%" }}
     >
-      <div id="potree_render_area" style={{
-    backgroundImage: "url('/images/loading.gif')"
-  }}/>
+      <div
+        id="potree_render_area"
+        style={{
+          backgroundImage: "url('/images/loading.gif')",
+        }}
+      />
       <div id="potree_sidebar_container" />
     </div>
   );
